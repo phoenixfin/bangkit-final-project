f471a67111be7629c38857366c2e2a52942e5a13
Anugerah Erlaut <aerlaut@live.com>
2020-05-25 20:33:26 +0700

    created basic UI

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello, Bangkit!"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
